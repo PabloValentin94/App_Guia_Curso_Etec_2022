5344e686279e455079e3cf708496ed9d86a8dcb1
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-05 15:57:33 -0300

    Adicionar arquivos de projeto.

---
+++ App_Guia_Curso_DS/App_Guia_Curso_DS/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Guia_Curso_DS
+{
+
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+        }
+
+        protected override void OnStart()
+        {
+
+
+
+        }
+
+        protected override void OnSleep()
+        {
+
+
+
+        }
+
+        protected override void OnResume()
+        {
+
+
+
+        }
+
+    }
+
+}
---
+++ App_Guia_Curso_DS/App_Guia_Curso_DS/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_Guia_Curso_DS
+{
+
+    public partial class MainPage : MasterDetailPage
+    {
+
+        public MainPage()
+        {
+
+            InitializeComponent();
+
+        }
+
+    }
+
+}
