0e204bdaadcf493005c5c0cedec6cb4055848d72
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-05 22:34:12 -0300

    Atualizando.

--- App_Guia_Curso_DS/App_Guia_Curso_DS.Android/MainActivity.cs
+++ App_Guia_Curso_DS/App_Guia_Curso_DS.Android/MainActivity.cs
@@ -12,11 +12,17 @@ namespace App_Guia_Curso_DS.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+            Window.AddFlags(Android.Views.WindowManagerFlags.DrawsSystemBarBackgrounds);
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#000000"));
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- App_Guia_Curso_DS/App_Guia_Curso_DS/MainPage.xaml.cs
+++ App_Guia_Curso_DS/App_Guia_Curso_DS/MainPage.xaml.cs
@@ -17,7 +17,97 @@ namespace App_Guia_Curso_DS
 
             InitializeComponent();
 
+            img_logo.Source = ImageSource.FromResource("App_Guia_Curso_DS.View.Images.logo.png");
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(InicialPage)));
+
         }
+
+        private void btn_ds_1_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_ds_2_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_ds_3_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        /*
+        private void btn_adm_1_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_adm_2_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_adm_3_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_meca_1_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_meca_2_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_meca_3_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_edif_1_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_edif_2_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void btn_edif_3_Clicked(object sender, EventArgs e)
+        {
+
+
+
+        }
+        */
 
     }
 
