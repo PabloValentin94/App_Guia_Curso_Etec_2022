5d1e8d70d404c415687499b1f6943c5137773efd
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-07 20:26:59 -0300

    Atualizando.

--- App_Guia_Curso_DS/App_Guia_Curso_DS/MainPage.xaml.cs
+++ App_Guia_Curso_DS/App_Guia_Curso_DS/MainPage.xaml.cs
@@ -19,7 +19,21 @@ namespace App_Guia_Curso_DS
 
             InitializeComponent();
 
+            // Definindo a imagem que aparecerá no menu.
+
             img_logo.Source = ImageSource.FromResource("App_Guia_Curso_DS.View.Images.logo.png");
+
+            /* Detail: propriedade de uma MasterDetailPage. No contexto, do método construtor, a estamos usando para específicar qual será
+             * a página que deverá ser acionada como página inicial (é tipo um Navigation.PushAsync(), só que da MasterDetailPage.). */
+
+            /* No comando abaixo ocorrem vários processos. Vamos às explicações:
+             *
+             *    - Definindo que a página instanciada será uma NavigationPage, ou seja, poderá navegar para outras
+             *      páginas (precisamos fazer isso para habilitarmos a função de troca de páginas.).
+             *
+             *    - Pegando o tipo do arquivo InicialPage.xaml e descobrindo se esse arquivo é equivalente a uma Page (página.).
+             *
+             *    - Caso o arquivo InicialPage.xaml seja equivalente a uma Page, então ele será instanciado/acionado/chamado. */
 
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(InicialPage)));
 
@@ -28,21 +42,36 @@ namespace App_Guia_Curso_DS
         private void btn_ds_1_Clicked(object sender, EventArgs e)
         {
 
+            /* Especificando a página que deverá ser acionada após o pressionamento do botão. Se necessário, revise as anotações presentes
+             * no método construtor. */
+
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Modulo_1)));
+
+            IsPresented = false;
 
         }
 
         private void btn_ds_2_Clicked(object sender, EventArgs e)
         {
 
+            /* Especificando a página que deverá ser acionada após o pressionamento do botão. Se necessário, revise as anotações presentes
+             * no método construtor. */
+
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Modulo_2)));
+
+            IsPresented = false;
 
         }
 
         private void btn_ds_3_Clicked(object sender, EventArgs e)
         {
 
+            /* Especificando a página que deverá ser acionada após o pressionamento do botão. Se necessário, revise as anotações presentes
+             * no método construtor. */
+
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Modulo_3)));
+
+            IsPresented = false;
 
         }
 
