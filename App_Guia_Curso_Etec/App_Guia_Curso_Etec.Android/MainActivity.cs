a7981edf044f3f203148e23044e67c1a262571d7
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-14 00:42:11 -0300

    Adicionando um ícone no aplicativo.

--- App_Guia_Curso_Etec/App_Guia_Curso_Etec.Android/MainActivity.cs
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace App_Guia_Curso_Etec.Droid
 {
-    [Activity(Label = "App_Guia_Curso_Etec", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "App_Guia_Curso_Etec", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- App_Guia_Curso_Etec/App_Guia_Curso_Etec.Android/Splash_Screen.cs
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec.Android/Splash_Screen.cs
@@ -13,7 +13,9 @@ namespace App_Guia_Curso_Etec.Droid
 {
     [Activity(Label = "App_Guia_Curso_Etec",
               Theme = "@style/MainTheme.SplashScreen", // Tema feito especificamente à Splash Screen.
-              MainLauncher = true,
+              MainLauncher = true, /* Define que essa activity é a principal. Como essa é a principal, devemos ir ao arquivo
+                                    * "MainActivity" e habilitar esta mesma propriedade como false, senão serão compilados, no
+                                    * celular, dois aplicativos (um com uma Splash Screen, e o outro, sem nenhuma.). */
               NoHistory = true /* Garante que se o usuário apertar o botão voltar, ele retornará à tela inicial do dispositivo,
                                 * e não à tela da Splash Screen. */)]
     public class Splash_Screen : Activity
