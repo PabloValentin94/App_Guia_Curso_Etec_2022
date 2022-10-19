388b4ac815fc3df5130d8ddab1ef922dbb687346
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-19 15:55:57 -0300

    Preparando o ambiente para atualizações.

---
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Bases_Tecnologicas.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
+{
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class DS_Bases_Tecnologicas : ContentPage
+    {
+
+        public DS_Bases_Tecnologicas()
+        {
+
+            InitializeComponent();
+
+        }
+
+    }
+
+}
