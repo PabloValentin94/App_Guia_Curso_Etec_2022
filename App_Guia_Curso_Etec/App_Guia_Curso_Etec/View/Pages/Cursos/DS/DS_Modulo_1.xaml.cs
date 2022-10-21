9c14b39e3e1b35896da056a71253081e395629cd
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-20 22:06:13 -0300

    Atualizando o primeiro módulo de Desenvolvimento de Sistemas.

--- App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_1.xaml.cs
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_1.xaml.cs
@@ -37,9 +37,9 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Carga_Horaria = 80,
 
-                    Atribuicoes_Empreendedoras = "",
-
                     Atribuicoes_Responsabilidades = "- Elaborar projetos de sistema de informação.",
+
+                    Atribuicoes_Empreendedoras = "- Analizar métodos de execução otimizados.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Estimular a organização;\n\n" +
@@ -54,7 +54,7 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
                                   "2.1 - Aplicar modelo Cascata no desenvolvimento de sistemas;\n\n" +
                                   "2.2 - Aplicar modelos ágeis e projetos de software.",
 
-                    Bases_Tecnologicas = "O conteúdo será colocado aqui."
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -86,11 +86,24 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Banco de Dados I",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Modelar banco de dados.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Estimular a organização;\n\n" +
                                        "- Promover ações que considerem o respeito às normas estabelecidas;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema.",
+
+                    Competencias = "1 - Desenvolver modelo de banco de dados.",
+
+                    Habilidades = "1.1 - Levantar a necessidade de informações do sistema;\n\n" +
+                                  "1.2 - Normalizar tabelas de banco de dados;\n\n" +
+                                  "1.3 - Associar tabelas para construção de banco de dados;\n\n" +
+                                  "1.4 - Aplicar a linguagem SQL na contrução de tabelas.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -122,12 +135,22 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Design Digital",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Desenvolver interfaces visuais para aplicativos e sites;\n\n" +
                                                     "- Desenvolver elementos gráficos para aplicativos e sites.",
 
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
+
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Incentivar ações que promovam a cooperação;\n\n" +
-                                       "- Respeitar as manifestações culturais de outros povos."
+                                       "- Respeitar as manifestações culturais de outros povos.",
+
+                    Competencias = "1 - Desenvolver interfaces visuais para aplicativos e sites.",
+
+                    Habilidades = "1.1 - Manipular e/ou construir elementos visuais para aplicativos e sites.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -159,11 +182,24 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Fundamentos da Informática",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Operar sistemas computacionais.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Desenvolver a criticidade;\n\n" +
                                        "- Incentivar comportamentos éticos;\n\n" +
-                                       "- Promover ações que considerem o respeito às normas estabelecidas."
+                                       "- Promover ações que considerem o respeito às normas estabelecidas.",
+
+                    Competencias = "1 - Articular conhecimentos de sistemas computacionais;\n\n" +
+                                   "2 - Distinguir sistemas computacionais.",
+
+                    Habilidades = "1.1 - Distinguir arquiteturas de sistemas de hardware e software;\n\n" +
+                                  "1.2 - Executar comandos em interface de linha de comando.\n\n" +
+                                  "2.1 - Utilizar sistemas computacionais.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -195,11 +231,23 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Programação Web I",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Desenvolver sites para a Web.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Estimular a organização;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema.",
+
+                    Competencias = "1 - Desenvolver páginas para a Internet.",
+
+                    Habilidades = "1.1 - Contruir páginas para a Internet, utilizando linguagem de marcação de texto;\n\n" +
+                                  "1.2 - Utlizar linguagem de script para a Web\n\n" +
+                                  "1.3 - Aplicar formatação através de folhas de estilo.\n\n",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -231,16 +279,29 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Técnicas de Programação e Algoritmos",
 
+                    Carga_Horaria = 120,
+
                     Atribuicoes_Responsabilidades = "- Verificar usabilidade no desenvolvimento de programas;\n\n" +
                                                     "- Realizar versionamento no desenvolvimento de programas;\n\n" +
                                                     "- Desenvolver programas de computador, utilizando princípios de boas práticas;\n\n" +
                                                     "- Implementar algoritmos em linguagem de programação, utilizando ambientes de " +
                                                     "desenvolvimento de acordo com as necessidades.",
 
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
+
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Incentivar atitudes de autonomia;\n\n" +
                                        "- Incentivar comportamentos éticos;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações-problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações-problema.",
+
+                    Competencias = "1 - Implementar algoritmos de programação;\n\n" +
+                                   "2 - Desenvolver sistemas, aplicando princípios e paradigmas de programação.",
+
+                    Habilidades = "1.1 - Elaborar algoritmos.\n\n" +
+                                  "2.1 - Codificar programas, utilizando técnicas de programação estruturada\n\n" +
+                                  "2.2 - Depurar e versionar programas, utilizando ambiente de desenvolvimento integrado.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
