cfb6a04fa13824a88ee88f3f05f8b5ad88621bc0
PABLO VALENTIN <pablo.valentin@etec.sp.gov.br>
2022-10-21 23:47:08 -0300

    Atualizando o segundo e o terceiro módulo de Desenvolvimento de Sistemas.

--- App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_2.xaml.cs
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_2.xaml.cs
@@ -37,11 +37,25 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Banco de Dados II",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Implementar bancos de dados.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Desenvolver a criticidade;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema.",
+
+                    Competencias = "1 - Implementar banco de dados relacional utilizando o Sistema Gerenciador de banco de dados;\n\n" +
+                                   "2 - Otimizar a linguagem de consulta estruturada como forma de informação relevante para a " +
+                                   "tomada de decisão.",
+
+                    Habilidades = "1.1 - Utilizar sistema de gerenciamento para banco de dados.\n\n" +
+                                  "2.1 - Executar linguagem de consulta estruturada objetivando melhor desempenho;\n\n" +
+                                  "2.2 - Compilar relatórios analíticos a partir dos dados coletados.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -73,13 +87,28 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Desenvolvimento de Sistemas",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Analisar e projetar sistemas de informação, selecionando linguagens de programação e ambientes " +
-                                                    "de desenvolvimento de acordo com as especificações do projeto;\n\n" +
+                                                    "de desenvolvimento de acordo com as especificidades do projeto;\n\n" +
                                                     "- Codificar e depurar programas.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes =  "- Fortalecer a persistência e o interesse na resolução de situações - problema;\n\n" +
                                         "- Incentivar ações que promovam a cooperação;\n\n" +
-                                        "- Desenvolver a criatividade."
+                                        "- Desenvolver a criatividade.",
+
+                    Competencias = "1 - Projetar sistemas de informação, selecionando linguagens de programação e ambientes " +
+                                   "de desenvolvimento de acordo com as especificidades do projeto.",
+
+                    Habilidades = "1.1 - Codificar programas orientados a objetos;\n\n" +
+                                  "1.2 - Utilizar ambientes de desenvolvimento para desenvolvimento desktop;\n\n" +
+                                  "1.3 - Conectar aplicações com banco de dados;\n\n" +
+                                  "1.4 - Aplicar técnicas de orientação a objetos;\n\n" +
+                                  "1.5 - Construir interface gráfica.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -111,11 +140,23 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Programação de Aplicativos Mobile I",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Elaborar projetos de aplicativos para platarformas móveis.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Responsabilizar-se pela produção, utilização e divulgação de informações;\n\n" +
-                                       "- Estimular a organização."
+                                       "- Estimular a organização.",
+
+                    Competencias = "1 - Projetar aplicativos, selecionando linguagens de programação e ambientes de desenvolvimento.",
+
+                    Habilidades = "1.1 - Utilizar ambientes de desenvolvimento de software mobile;\n\n" +
+                                  "1.2 - Construir interface gráfica para aplicativos mobile;\n\n" +
+                                  "1.3 - Utilizar recursos de aparelhos celulares e tablets.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -147,11 +188,24 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Programação Web II",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Planejar projetos de sistemas de informação para a Web.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Responsabilizar-se pela produção, utilização e divulgação de informações;\n\n" +
                                        "- Fortalecer a persistência e o interesse na resolução de situações - problema;\n\n" +
-                                       "- Incentivar a criatividade."
+                                       "- Incentivar a criatividade.",
+
+                    Competencias = "1 - Desenvolver sistemas para internet utilizando persistência em banco de dados, " +
+                                   "interface com ousuário e programação em lado servidor.",
+
+                    Habilidades = "1.1 - Codificar software em linguagem para Web;\n\n" +
+                                  "1.2 - Utilizar banco de dados relacionais para persistência dos dados;\n\n" +
+                                  "1.3 - Utilizar interface baseada em navegador para interação com usuário.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -183,11 +237,24 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Sistemas Embarcados",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Desenvolver sistemas embarcados.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Fortalecer a persistência e o interesse na resolução de situações - problema;\n\n" +
                                        "- Estimular a organização;\n\n" +
-                                       "- Incentivar a criatividade."
+                                       "- Incentivar a criatividade.",
+
+                    Competencias = "1 - Analisar modelos de sistemas embarcados;\n\n" +
+                                   "2 - Desenvolver aplicações com microcontroladores.",
+
+                    Habilidades = "1.1 - Identificar as características de sistemas embarcados.\n\n" +
+                                  "2.1 - Programar sistemas para microcontroladores;\n\n" +
+                                  "2.2 - Executar instruções para microcontroladores.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
--- App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_3.xaml.cs
+++ App_Guia_Curso_Etec/App_Guia_Curso_Etec/View/Pages/Cursos/DS/DS_Modulo_3.xaml.cs
@@ -35,12 +35,30 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Internet, Protocolos e Segurança de Sistemas da Informação",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Implementar rotinas de segurança da informação;\n\n" +
                                                     "- Utilizar protocolos de redes e internet para comunicação de dados.",
 
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
+
                     Valores_Atitudes = "- Incentivar comportamentos éticos;\n\n" +
                                        "- Promover ações que considerem o respeito às normas estabelecidas;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema.",
+
+                    Competencias = "1 - Configurar os principais serviços de redes de comunicação de dados e internet " +
+                                   "para o desenvolvimento de sistemas;\n\n" +
+                                   "2 - Desenvolver sistemas implementando rotinas de segurança de dados.",
+
+                    Habilidades = "1.1 - Identificar modelo de referência de arquitetura de redes de comunicação de " +
+                                  "dados e internet paraa escolha de protocolos adequados aos sistemas em desenvolvimento;\n\n" +
+                                  "1.2 - Utilizar protocolos de rede e de comunicação de dados que auxiliem no " +
+                                  "desenvolvimento de sistemas.\n\n" +
+                                  "2.1 - Identificar ameaças à segurança da informação;\n\n" +
+                                  "2.2 - Utilizar técnicas de segurança da informação;\n\n" +
+                                  "2.3 - Operar mecanismos de segurança da informação no desenvolvimento de sistemas.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -73,12 +91,71 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
                     Nome = "Planejamento e Desenvolvimento do Trabalho de Conclusão de Curso (T.C.C.) " +
                            "em Desenvolvimento de Sistemas",
 
+                    Carga_Horaria = 120,
+
                     Atribuicoes_Responsabilidades = "- Planejar e desenvolver projetos de sistemas computacionais.",
 
-                    Valores_Atitudes = "- Comunicar-se com a equipe com clareza e objetividade;\n\n" +
-                                       "- Demonstrar comprometimento com a equipe e o trabalho;\n\n" +
-                                       "- Planejar ações mais eficazes no desenvolvimento de sistemas;\n\n" +
-                                       "- Organizar procedimentos de maneira diversa, visando melhor eficiência."
+                    Atribuicoes_Empreendedoras = "- Comunicar-se com a equipe com clareza e objetividade;\n\n" +
+                                                 "- Demonstrar comprometimento com a equipe e o trabalho;\n\n" +
+                                                 "- Planejar ações mais eficazes no desenvolvimento de sistemas;\n\n" +
+                                                 "- Organizar procedimentos de maneira diversa, visando melhor eficiência.",
+
+                    Valores_Atitudes = "- Estimular a organização;\n\n" +
+                                       "- Incentivar comportamentos éticos;\n\n" +
+                                       "- Responsabilizar-se pela produção, utilização e divulgação de informações.",
+
+                    Competencias = "1º Semestre:\n\n" +
+
+                                   "1 - Analisar dados e informações obtidas de pesquisas empíricas e bibliográficas;\n\n" +
+                                   "2 - Propor soluções parametrizadas por viabilidade técnica e econômica aos " +
+                                   "problemas identificados no âmbito da área profissional;\n\n" +
+                                   "3 - Correlacionar a formação técnica às demandas do setor produtivo voltadas " +
+                                   "para gestão ambiental e Segurança do Trabalho;\n\n" +
+                                   "4 - Construir projeto de software.\n\n" +
+                                   
+                                   "2º Semestre:\n\n" +
+
+                                   "1 - Planejar as fases de execução de projetos com base na natureza e na complexidade " +
+                                   "das atividades;\n\n" +
+                                   "2 - Avaliar as fontes e recursos necessários para o desenvolvimento de projetos;\n\n" +
+                                   "3 - Avaliar a execução e os resultados obtidos de forma quantitativa e qualitativa;\n\n" +
+                                   "4 - Utilizar princípios inovadores de Empreendedorismo na criação de projetos / " +
+                                   "startups de tecnologia;\n\n" +
+                                   "5 - Documentar sistemas de informação.",
+
+                    Habilidades = "1º Semestre:\n\n" +
+
+                                  "1.1 - Identificar demandas e situações-problema no âmbito da área profissional;\n\n" +
+                                  "1.2 - Identificar fontes de pesquisa sobre o objeto em estudo;\n\n" +
+                                  "1.3 - Elaborar instrumentos de pesquisa para desenvolvimento de projetos;\n\n" +
+                                  "1.4 - Constituir amostras para pesquisas técnicas e científicas, de forma criteriosa e explicitada;\n\n" +
+                                  "1.5 - Aplicar instrumentos de pesquisa de campo.\n\n" +
+                                  "2.1 - Consultar legislação, normas e regulamentos relativos ao projeto;\n\n" +
+                                  "2.2 - Registrar as etapas do trabalho;\n\n" +
+                                  "2.3 - Organizar os dados obtidos na forma de textos, planilhas, gráficos e esquemas.\n\n" +
+                                  "3.1 - Consultar legislação, normas e regulamentos relativos ao projeto.\n\n" +
+                                  "4.1 - Elaborar modelo de negócio para uma empresa de software;\n\n" +
+                                  "4.2 - Articular conhecimentos de empreendedorismo na construção de projetos de software.\n\n" +
+
+                                  "2º Semestre:\n\n" +
+
+                                  "1.1 - Consultar diversas fontes de pesquisa: catálogos, manuais de fabricantes, " +
+                                  "glossários técnicos, entre outros;\n\n" +
+                                  "1.2 - Comunicar ideias de forma clara e objetiva por meio de textos escritos " +
+                                  "e de explanações orais.\n\n" +
+                                  "2.1 - Definir recursos necessários e plano de produção;\n\n" +
+                                  "2.2 - Classificar os recursos necessários para o desenvolvimento do projeto;\n\n" +
+                                  "2.3 - Utilizar, de modo racional, os recursos destinados ao projeto.\n\n" +
+                                  "3.1 - Verificar e acompanhar o desenvolvimento do cronograma físico - financeiro;\n\n" +
+                                  "3.2 - Redigir relatórios sobre o desenvolvimento do projeto;\n\n" +
+                                  "3.3 - Construir gráficos, planilhas, cronogramas e fluxogramas;\n\n" +
+                                  "3.4 - Organizar informações, textos e dados conforme formatação definida.\n\n" +
+                                  "4.1 - Elaborar proposta de projeto de conclusão de curso / startup;\n\n" +
+                                  "4.2 - Articular conhecimentos de Empreendedorismo.\n\n" +
+                                  "5.1 - Elaborar diagramas na linguagem de modelagem unificada;\n\n" +
+                                  "5.2 - Indicar utilização adequada do sistema projetado.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -110,11 +187,25 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Programação de Aplicativos Mobile II",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Documentar, construir e manter sistemas de informação para plataformas móveis.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Estimular a organização;\n\n" +
-                                       "- Responsabilizar-se pela produção, utilização e divulgação de informações."
+                                       "- Responsabilizar-se pela produção, utilização e divulgação de informações.",
+
+                    Competencias = "1 - Projetar aplicativos selecionando linguagens de programação e " +
+                                   "ambientes de desenvolvimento.",
+
+                    Habilidades = "1.1 - Utilizar ambientes de desenvolvimento mobile;\n\n" +
+                                  "1.2 - Elaborar aplicativos com acesso a banco de dados;\n\n" +
+                                  "1.3 - Construir layout de aplicativos para dispositivos móveis;\n\n" +
+                                  "1.4 - Utilizar recursos avançados do dispositivo (smartphones e tablets).",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -146,11 +237,25 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Programação Web III",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Documentar, construir e manter sistemas de informação para Web.",
+
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
 
                     Valores_Atitudes = "- Incentivar a criatividade;\n\n" +
                                        "- Responsabilizar-se pela produção, utilização e divulgação de informações;\n\n" +
-                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema."
+                                       "- Fortalecer a persistência e o interesse na resolução de situações - problema.",
+
+                    Competencias = "1 - Desenvolver sistemas multicamadas, utilizando framework de desenvolvimento Web;\n\n" +
+                                   "2 - Desenvolver serviços para o usuário utilizando recursos dos dispositivos móveis.",
+
+                    Habilidades = "1.1 - Utilizar conjunto de bibliotecas (framework) para o desenvolvimento Web.\n\n" +
+                                  "2.1 - Utilizar recursos dos dispositivos móveis na integração de aplicativos " +
+                                  "à Internet;\n\n" +
+                                  "2.2 - Construir aplicativos para a Internet.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
@@ -182,12 +287,23 @@ namespace App_Guia_Curso_Etec.View.Pages.Cursos.DS
 
                     Nome = "Qualidade e Teste de Software",
 
+                    Carga_Horaria = 80,
+
                     Atribuicoes_Responsabilidades = "- Testar softwares para melhoria da qualidade de sistemas;\n\n" +
                                                     "- Elaborar registros e planilhas de acompanhamento e controle das atividades.",
 
+                    Atribuicoes_Empreendedoras = "- Nenhuma.",
+
                     Valores_Atitudes = "- Desenvolver a criticidade;\n\n" +
                                        "- Incentivar comportamentos éticos;\n\n" +
-                                       "- Responsabilizar-se pela produção, utilização e divulgação de informações."
+                                       "- Responsabilizar-se pela produção, utilização e divulgação de informações.",
+
+                    Competencias = "1 - Avaliar e selecionar técnicas de teste de software.",
+
+                    Habilidades = "1.1 - Utilizar softwares de apoio ao teste de sistemas;\n\n" +
+                                  "1.2 - Verificar e validar correspondência entre a especificação e o produto testado.",
+
+                    Bases_Tecnologicas = "- O conteúdo será colocado aqui."
 
                 };
 
